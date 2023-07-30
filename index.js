88abd75bf07c4b6f6f18a880884af0bb0a9e5c8d
SumarPohz <sumarpohz@gmail.com>
2023-07-31 02:09:53 +0530

    submit Initial commit

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 let paymentButtonClicked = false;
 let paymentScreenshotUploaded = false;
 
-// Show the payment popup
 function showPaymentPopup() {
   document.getElementById("payment-popup").style.display = "block";
 }
 
-// Close the payment popup
 function closePaymentPopup() {
   document.getElementById("payment-popup").style.display = "none";
 }
 
-// Handle payment button click
 document.getElementById("payment-btn").addEventListener("click", function() {
   paymentButtonClicked = true;
 });
-
-// Handle payment screenshot upload
+ 
 document.getElementById("payment-screenshot").addEventListener("change", function() {
   paymentScreenshotUploaded = this.files.length > 0;
 });
+

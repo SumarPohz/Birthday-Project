24dfee3b7e10daab792c8067c12862b5bcd5b247
SumarPohz <sumarpohz@gmail.com>
2023-07-30 23:19:58 +0530

    UPDATE

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let paymentButtonClicked = false;
+let paymentScreenshotUploaded = false;
+
 // Show the payment popup
 function showPaymentPopup() {
   document.getElementById("payment-popup").style.display = "block";
@@ -8,11 +11,24 @@ function closePaymentPopup() {
   document.getElementById("payment-popup").style.display = "none";
 }
 
-// Disable form submission until the payment button is clicked
+// Handle payment button click
+document.getElementById("payment-btn").addEventListener("click", function() {
+  paymentButtonClicked = true;
+});
+
+// Handle payment screenshot upload
+document.getElementById("payment-screenshot").addEventListener("change", function() {
+  paymentScreenshotUploaded = this.files.length > 0;
+});
+
+// Handle form submission
 document.getElementById("registration-form").addEventListener("submit", function(event) {
-  // Check if the payment button is required and clicked
-  if (!document.getElementById("payment-btn").clicked) {
+  // Check if the payment button is clicked and screenshot is uploaded
+  if (!paymentButtonClicked || !paymentScreenshotUploaded) {
     event.preventDefault();
-    alert("Please make the payment before submitting the form.");
+    alert("Please make the payment and upload the screenshot before submitting the form.");
+  } else {
+    // Form validation passed, proceed with form submission
+    // You can also add further processing here if needed
   }
 });

7b49799dc03ad2a4587da9864cc00cc3ac26b45e
SumarPohz <sumarpohz@gmail.com>
2023-08-19 14:48:00 +0530

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+const addInviteButton = document.getElementById('addInvite');
+    const inviteContainer = document.getElementById('inviteContainer');
+
+    addInviteButton.addEventListener('click', () => {
+        const newInviteEntry = document.createElement('div');
+        newInviteEntry.className = 'invite-entry';
+        newInviteEntry.innerHTML = `
+            <input type="text" name="invitedName[]" placeholder="Friend's Number">
+            <select name="friendPlan[]">
+                <option value="Banana(Rs.577)">Banana(Rs.577)</option>
+                <option value="Pomegranates(Rs.3300)">Pomegranates(Rs.3300)</option>
+                <option value="Kiwi Investment">Kiwi(Rs.7800)</option>
+                <option value="Dragon Fruit(Rs.14500)">Dragon Fruit(Rs.14500)</option>
+            </select>
+        `;
+        inviteContainer.appendChild(newInviteEntry);
+    });

2763fdd3f3cc51995133ce26c1ff5895b627a995
SumarPohz <sumarpohz@gmail.com>
2023-08-25 01:35:30 +0530

    Commit Initial Update

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ function showForm(formId) {
     } else {
       form.style.display = "none";
     }
-  }
+}
+function showForm(formId) {
+    var form = document.getElementById(formId);
+    if (form.style.display === "none") {
+      form.style.display = "block";
+    } else {
+      form.style.display = "none";
+    }
+}

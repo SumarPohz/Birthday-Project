8800ffd81711c319fa6285143801c522c582f288
Sumarbha Pohsnem <140301578+SumarPohz@users.noreply.github.com>
2023-08-25 01:04:20 +0530

    Commit Initial Updates

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-const addInviteButton = document.getElementById('addInvite');
-    const inviteContainer = document.getElementById('inviteContainer');
-
-    addInviteButton.addEventListener('click', () => {
-        const newInviteEntry = document.createElement('div');
-        newInviteEntry.className = 'invite-entry';
-        newInviteEntry.innerHTML = `
-            <input type="text" name="Invited Number[]" placeholder="Friend's Number">
-            <select name="friendPlan[]">
-                <option value="Banana(Rs.577)">Banana(Rs.577)</option>
-                <option value="Pomegranates(Rs.3300)">Pomegranates(Rs.3300)</option>
-                <option value="Kiwi Investment">Kiwi(Rs.7800)</option>
-                <option value="Dragon Fruit(Rs.14500)">Dragon Fruit(Rs.14500)</option>
-            </select>
-        `;
-        inviteContainer.appendChild(newInviteEntry);
-    });
+function showForm(formId) {
+    var form = document.getElementById(formId);
+    if (form.style.display === "none") {
+      form.style.display = "block";
+    } else {
+      form.style.display = "none";
+    }
+  }
